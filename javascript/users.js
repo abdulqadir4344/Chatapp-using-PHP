385433b7411314eddfc783d758012d636dfdba48
abdul <abdulqadir4344@gmail.com>
2024-09-28 19:05:33 +0530

    add menubar

--- javascript/users.js
+++ javascript/users.js
@@ -49,3 +49,16 @@ setInterval(() =>{
   xhr.send();
 }, 500);
 
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.style.display === "block") {
+              openDropdown.style.display = "block";
+          }
+      }
+  }
+}
--- users.php
+++ users.php
@@ -6,11 +6,35 @@
     header("location: login.php");
   }
 ?>
+<?php 
+  $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+  if(mysqli_num_rows($sql) > 0){
+    $row = mysqli_fetch_assoc($sql);
+  }
+?>
 <?php include_once "header.php"; ?>
 <body>
+
 <!-- <i class="fa-solid fa-badge-check" style="color: #0084ff;"></i> -->
   <div class="wrapper">
+  <navbar class="navbar">
+        <h1>iTalk</h1>
+        <div class="icons">
+            <i class="fas fa-qrcode icon" title="QR Scanner"></i>
+            <i class="fas fa-camera icon" title="Camera"></i>
+            <div class="dropdown">
+        <button class="dropbtn"> <i class="fas fa-ellipsis-v icon" title="More Options"></i></button>
+        <div class="dropdown-content">
+            <a href="#">New Chat</a>
+            <a href="#">New Group</a>
+            <a href="setting.php">Settings</a>
+            <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>">Logout</a>
+        </div>
+    </div>
+        </div>
+      </navbar>
     <section class="users">
+ 
       <header>
         <div class="content">
           <?php 
